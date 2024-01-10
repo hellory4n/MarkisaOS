4b45575d51a992fca5a4f61f00692d727c3e1634
PotatopatataCodes <potatopatatagames@gmail.com>
2024-01-10 13:20:38 -0500

    now it uses cairo instead of opengl lol

--- assets/testVertexShader.vert
+++
@@ -1,7 +0,0 @@
-#version 300 es
-
-layout(location = 0) in vec4 position;
-
-void main() {
-    gl_Position = position;
-}
--- src/Main.hx
+++ src/Main.hx
@@ -1,33 +1,17 @@
 package;
 
-import lime.utils.Float64Array;
-import lime.graphics.WebGLRenderContext;
+import lime.graphics.cairo.CairoImageSurface;
 import frambos.core.Assets;
 import frambos.core.Assets.Texture;
 import frambos.ecs.Block;
 import frambos.ecs.BlockTree;
 import markisa.bootloader.Boot;
 import lime.app.Application;
-import lime.graphics.opengl.GLBuffer;
-import lime.graphics.opengl.GLProgram;
-import lime.graphics.opengl.GLTexture;
-import lime.graphics.opengl.GLUniformLocation;
 import lime.graphics.RenderContext;
 
 class Main extends Application {
-	var glBuffer: GLBuffer;
-	var glMatrixUniform: GLUniformLocation;
-	var glProgram: GLProgram;
-	var glTexture: GLTexture;
-	var glTextureAttribute: Int;
-	var glVertexAttribute: Int;
 	var image: Texture;
-	var timeToSetupOpenGeeEl = true;
-	var positionsAndShit = new Float64Array(6, null, [
-		-0.5, -0.5,
-		 0.0,  0.5,
-		 0.5, -0.5
-	]);
+	var cairoSurface: CairoImageSurface;
 	
 	public function new() {
 		super();
@@ -52,68 +36,35 @@ class Main extends Application {
 		BlockTree.callDraw();
 		
 		switch (context.type) {
-			case OPENGL, OPENGLES:
-				var gl = context.webgl;
-				trace("time to render lmao");
+			case CAIRO:
+				var cairo = context.cairo;
 				
-				if (preloader.complete) {
-					trace("shit has been preloaded");
-					if (timeToSetupOpenGeeEl) {
-						trace("setting up opengl");
-						timeToSetupOpenGl(gl);
-						trace("opengl has been setup");
-
-						timeToSetupOpenGeeEl = false;
-						trace("setup is done");
-					} else {
-						trace("time to render bullshit");
-						renderStuffAndStuff(gl, context);
-						trace("bullshit has been rendered");
-					}
-				} else {
-					trace("no shit preloaded :(");
+				if (image == null && preloader.complete) {
+					image = Assets.loadTexture("assets/icon.png");
+					image.format = BGRA32;
+					image.premultiplied = true;
+					
+					cairoSurface = CairoImageSurface.fromImage(image);
 				}
+				
+				var r = ((context.attributes.background >> 16) & 0xFF) / 0xFF;
+				var g = ((context.attributes.background >> 8) & 0xFF) / 0xFF;
+				var b = (context.attributes.background & 0xFF) / 0xFF;
+				var a = ((context.attributes.background >> 24) & 0xFF) / 0xFF;
+				
+				cairo.setSourceRGB(r, g, b);
 
-				trace("ok done");
+				cairo.paint();
+				
+				if (image != null) {
+					image.format = BGRA32;
+					image.premultiplied = true;
+					
+					cairo.setSourceSurface(cairoSurface, 0, 0);
+					cairo.paint();
+				}
 			
 			default:
 		}
-	}
-
-	function timeToSetupOpenGl(gl: WebGLRenderContext) {
-		// awesome opengl setup bullshit
-		glBuffer = gl.createBuffer();
-		
-		// give opengl the triangle
-		trace("buffering data");
-		gl.bufferData(gl.ARRAY_BUFFER, positionsAndShit, gl.STATIC_DRAW);
-
-		trace("enabling vertex attribute array");
-		gl.enableVertexAttribArray(0);
-		// tell opengl what does our awesome array mean
-		trace("setting vertex attribute pointer");
-		gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 8, 0);
-
-		// quite the mouthful
-		var awesomeShader = GLProgram.fromSources(gl, Assets.loadText("res://assets/testVertexShader.vert"),
-													  Assets.loadText("res://assets/testFragmentShader.frag"));
-		
-		gl.useProgram(awesomeShader);
-	}
-
-	function renderStuffAndStuff(gl: WebGLRenderContext, context: RenderContext) {
-		var r = ((context.attributes.background >> 16) & 0xFF) / 0xFF;
-		var g = ((context.attributes.background >> 8) & 0xFF) / 0xFF;
-		var b = (context.attributes.background & 0xFF) / 0xFF;
-		var a = ((context.attributes.background >> 24) & 0xFF) / 0xFF;
-
-		gl.viewport(0, 0, window.width, window.height);
-		gl.clearColor(r, g, b, a);
-		gl.clear(gl.COLOR_BUFFER_BIT);
-		
-		gl.bindBuffer(gl.ARRAY_BUFFER, glBuffer);
-
-		trace("drawing arrays");
-		gl.drawArrays(gl.TRIANGLES, 0, 3);
 	}
 }
