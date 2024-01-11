d226c2c0f3473631f5c4dd490bbfef55c4409552
hellory4n <hellory4n@gmail.com>
2024-01-11 16:56:51 -0500

    updated tests

--- src/markisa/bootloader/Boot.hx
+++ src/markisa/bootloader/Boot.hx
@@ -1,5 +1,6 @@
 package markisa.bootloader;
 
+import markisa.engineTest.OtherTestPiece;
 import markisa.engineTest.TestPiece;
 import frambos.ecs.BlockTree;
 import frambos.ecs.Block;
@@ -8,31 +9,14 @@ class Boot {
     public function new() {
         trace("the game started omgomgmogmogmomogmogmogmogmo");
 
+        var vmdgdgkd = new Block();
+        vmdgdgkd.name = "vmdgdgkd";
+        vmdgdgkd.getPiece(OtherTestPiece);
+        BlockTree.root.addChild(vmdgdgkd);
+
         var heheheha = new Block();
         heheheha.name = "heheheha";
         heheheha.getPiece(TestPiece);
         BlockTree.root.addChild(heheheha);
-
-        var haha = new Block();
-        haha.name = "haha";
-        BlockTree.root.addChild(haha);
-
-        var comedy = new Block();
-        comedy.name = "comedy";
-        heheheha.addChild(comedy);
-
-        var amogus = new Block();
-        amogus.name = "amogus";
-        heheheha.addChild(amogus);
-
-        var itIsThePinnacleOfComedy = new Block();
-        itIsThePinnacleOfComedy.name = "itIsThePinnacleOfComedy";
-        amogus.addChild(itIsThePinnacleOfComedy);
-
-        trace(heheheha.getPath());
-        trace(haha.getPath());
-        trace(comedy.getPath());
-        trace(amogus.getPath());
-        trace(itIsThePinnacleOfComedy.getPath());
     }
 }
--- src/markisa/engineTest/OtherTestPiece.hx
+++ src/markisa/engineTest/OtherTestPiece.hx
@@ -6,26 +6,18 @@ import frambos.core.Assets;
 import frambos.graphics.RenderDevice;
 import frambos.ecs.Piece;
 
-class TestPiece extends Piece {
+class OtherTestPiece extends Piece {
     var fuckingSurface: ImageSurface;
 
     public override function ready() {
         trace("good morning lmao");
-        size = new Vec2(690, 420);
+        size = new Vec2(1366, 768);
         trace("i'm ready, i'm ready!!!!!!!!!!!");
-    }
-
-    public override function update(delta: Float) {
-        trace("updating");
-        position += new Vec2(50 * delta, 50 * delta);
-        rotation += 200 * delta;
-        alpha -= 0.25 * delta;
-        trace("updated");
     }
 
     public override function prepareDraw(device: RenderDevice) {
         trace("preparing draw");
-        var awesomeTexture: Texture = Assets.loadTexture("res://assets/markisa.png");
+        var awesomeTexture: Texture = Assets.loadTexture("res://assets/greenPeaks.png");
         fuckingSurface = device.newImageSurface(awesomeTexture);
         trace("draw has been prepared");
     }
