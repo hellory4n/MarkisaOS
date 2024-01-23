6b4a2374c84c75c174e1ef23d05471c0af90d53b
hellory4n <hellory4n@gmail.com>
2024-01-22 20:04:33 -0500

    sound mangngnejrtjdtjiogiogtioidt & shit

---
+++ os/dashboardtk/sound_player.gd
@@ -0,0 +1,21 @@
+## It plays a sound effect. Use MusicPlayer if you want music.
+extends Node
+class_name SoundPlayer
+
+## The AudioStream object to be played.
+@export var stream: AudioStream
+## If true, audio plays when added to scene tree.
+@export var autoplay := false
+var internal_player: AudioStreamPlayer
+
+func _ready():
+	if autoplay:
+		play()
+
+## Plays the audio.
+func play():
+	internal_player = SoundManager.play_sound(stream)
+
+## Stops the audio.
+func stop():
+	internal_player.stop()
---
+++ os/dashboardtk/system_sound_player.gd
@@ -0,0 +1,30 @@
+extends SoundPlayer
+class_name SystemSoundPlayer
+
+enum SystemSounds {
+	STARTUP,
+	SHUTDOWN,
+	LOGOUT,
+	WARNING,
+	ERROR,
+	NOTIFICATION,
+	CRITICAL_ERROR,
+	QUESTION,
+	SUCCESS
+}
+
+@export var sound := SystemSounds.STARTUP
+
+func _ready():
+	match sound:
+		SystemSounds.STARTUP: stream = preload("res://os_assets/system_sounds/startup.mp3")
+		SystemSounds.SHUTDOWN: stream = preload("res://os_assets/system_sounds/shutdown.mp3")
+		SystemSounds.LOGOUT: stream = preload("res://os_assets/system_sounds/logout.mp3")
+		SystemSounds.WARNING: stream = preload("res://os_assets/system_sounds/warning.mp3")
+		SystemSounds.ERROR: stream = preload("res://os_assets/system_sounds/error.mp3")
+		SystemSounds.NOTIFICATION: preload("res://os_assets/system_sounds/notification.mp3")
+		SystemSounds.CRITICAL_ERROR: preload("res://os_assets/system_sounds/critical_error.mp3")
+		SystemSounds.QUESTION: stream = preload("res://os_assets/system_sounds/question.mp3")
+		SystemSounds.SUCCESS: stream = preload("res://os_assets/system_sounds/success.mp3")
+	
+	super()
