e36edf6f1c2ec7d2dfc64644422f360c17e7fbda
hellory4n <hellory4n@gmail.com>
2024-01-30 16:47:51 -0500

    Fusdfuiujgsetuisdfuud

--- os/kickstart/Frambos.cs
+++ os/kickstart/Frambos.cs
@@ -3,12 +3,23 @@ using System;
 
 namespace markisa.foundation
 {
+
 public class Frambos : Node
 {
     public static Vector2 Resolution { get; set; } = new Vector2(853, 480);
-    public static bool IsOnMobile => OS.GetName() == "Android";
+    public static bool IsOnMobile => OS.GetName() == "Android" || forceMobile;
     public static string CurrentUser { get; set; } = "";
     public static string CurrentUserDisplayName { get; set; } = "";
-    //static PackedScene notificationShit = GD.Load<PackedScene>()
+    static bool forceMobile = false;
+        //static PackedScene notificationShit = GD.Load<PackedScene>()
+
+    public override void _Ready()
+    {
+        var dir = new Directory();
+        if (dir.FileExists("user://forcemobile")) {
+            forceMobile = true;
+        }
+    }
 }
+
 }
