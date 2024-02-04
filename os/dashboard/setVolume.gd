98cf723edb6773b2759dd6cd8bc62d35271f0cd2
hellory4n <hellory4n@gmail.com>
2024-02-04 15:30:48 -0500

    there you go

--- os/dashboard/setVolume.gd
+++ os/dashboard/setVolume.gd
@@ -1,15 +1,20 @@
 extends HSlider
 
-export var sound := false
+export var mode := 0
 
 func _on_HSlider_value_changed(value):
-	if sound:
+	if mode == 0:
 		AudioServer.set_bus_volume_db(
 			AudioServer.get_bus_index("sound"),
 			linear2db(value)
 		)
-	else:
+	elif mode == 1:
 		AudioServer.set_bus_volume_db(
 			AudioServer.get_bus_index("music"),
+			linear2db(value)
+		)
+	else:
+		AudioServer.set_bus_volume_db(
+			AudioServer.get_bus_index("pc"),
 			linear2db(value)
 		)
