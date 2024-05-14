8e93d6fca69ccd39b751b8f5f4cbe102e9e176fe
hellory4n <hellory4n@gmail.com>
2024-05-13 22:59:22 -0500

    E. P. Thompson

---
+++ os/kickstart/network/WebIndexer.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+[Tool]
+public class WebIndexer : Node
+{
+    public override void _Ready()
+    {
+        #if DEBUG
+        // i release beta versions on saturday, i need to update the indexing
+        if (DateTime.Now.DayOfWeek == DayOfWeek.Saturday) {
+            return;
+        }
+
+        OS.Execute("zenity", new string[] { "--warning", "--text=\"\"UPDATE THE WEBSITE INDEX!!!!1\"\"" });
+
+        // update the index :)
+        string filename = "";
+        var dir = new Directory();
+        dir.ListDirBegin(true);
+        while (filename != "") {
+            // add stuff to open the scene but just for the tags lol
+            filename = dir.GetNext();
+        }
+        dir.ListDirEnd();
+        #endif
+    }
+}
