9a0700eefb5c81ecf05462a8906019be489dbac8
hellory4n <hellory4n@gmail.com>
2024-06-03 23:15:08 -0500

    connect deez nuts

--- social/connect/TrendingZone1.cs
+++ social/connect/TrendingZone1.cs
@@ -84,7 +84,7 @@ public static MksConnectZone Data { get; set; } = new MksConnectZone {
 
         new MksPost {
             User = "Daily Thoughts",
-            ProfilePicture = "social/connectpeople/dailyThoughts.png",
+            ProfilePicture = "res://social/connectpeople/dailyThoughts.png",
             Content = "if i kill my neighbor it's murder, if i kill the president it's an assassination",
             Replies = new MksPost[] {
                 new MksPost {
@@ -103,6 +103,162 @@ public static MksConnectZone Data { get; set; } = new MksConnectZone {
                     User = "Ross Smith",
                     ProfilePicture = "res://os/assets/userIcons/duck.png",
                     Content = "?"
+                },
+            }
+        },
+        
+        new MksPost {
+            User = "Daily Thoughts",
+            ProfilePicture = "res://social/connectpeople/dailyThoughts.png",
+            Content = "imagine if we're'all just virtual people on a game with a dumb idea",
+            Replies = new MksPost[] {
+                new MksPost {
+                    User = "Rob Smith",
+                    ProfilePicture = "res://os/assets/userIcons/duck.png",
+                    Content = "RELATABLE"
+                },
+            }
+        },
+
+        new MksPost {
+            User = "Daily Thoughts",
+            ProfilePicture = "res://social/connectpeople/dailyThoughts.png",
+            // translators: this can be any tongue twister, doesn't matter
+            Content = "How much wood would a woodchuck chuck if a woodchuck could chuck wood?",
+            Replies = new MksPost[] {
+                new MksPost {
+                    User = "John A. Melton",
+                    ProfilePicture = "res://os/assets/userIcons/dog.png",
+                    Content = "Haven't thought about that, I agree honestly"
+                },
+            }
+        },
+
+        new MksPost {
+            User = "John A. Melton",
+            ProfilePicture = "res://os/assets/userIcons/dog.png",
+            Content = "Just noticed i have 206470572039523 downloads :)",
+            Replies = new MksPost[] {
+                new MksPost {
+                    User = "Faster PC",
+                    ProfilePicture = "res://social/connectpeople/fasterpc.png",
+                    Content = "Wow, browsing through 206470572039523 downloads must be tricky, IF ONLY THERE WAS AN APP THAT HELPED WITH THAT (it's fasterpc)",
+                    Link = "fasterpc.com"
+                },
+
+                new MksPost {
+                    User = "Rob Smith",
+                    ProfilePicture = "res://os/assets/userIcons/duck.png",
+                    // translators: it's supposed to clip at the end
+                    Content = "<ping>Faster PC</ping> don't download it's a virus don't download it's a virus don't download it's a virus don't download it's a virus don't download it's a virus don't download it's a virus don't download it's a virus don't download it's a virus don't download it's a virus don't download it's a virus don't download it's a virus don't download it's a virus don't download it's a virus don't download it's a virus don't download it's a virus don't download it's a virus don't download it's a virus don't download it's a virus don't download it's a virus don't download it's a virus don't download it's a virus don't download it's a virus don't download it's a virus don't download it's a virus don't download it's a virus don't download it's a virus don't downlo"
+                },
+            }
+        },
+
+        new MksPost {
+            User = "Daniel McLeinad",
+            ProfilePicture = "res://os/assets/userIcons/pancakes.png",
+            Content = "eating a pizza, i'm miserable",
+            Replies = new MksPost[] {
+                new MksPost {
+                    User = "Faster PC",
+                    ProfilePicture = "res://social/connectpeople/fasterpc.png",
+                    Content = "You know what's also miserable? YOUR COMPUTER WITH ALL THAT GARBAGE! DOWNLOAD FASTERPC TODAY",
+                    Link = "fasterpc.com"
+                },
+
+                new MksPost {
+                    User = "Rob Smith",
+                    ProfilePicture = "res://os/assets/userIcons/duck.png",
+                    Content = "relatable (also fasterpc is a virus)"
+                },
+            }
+        },
+
+        new MksPost {
+            User = "Daniel McLeinad",
+            ProfilePicture = "res://os/assets/userIcons/pancakes.png",
+            Content = "still eating a pizza, i'm still miserable",
+            Replies = new MksPost[] {
+                new MksPost {
+                    User = "Faster PC",
+                    ProfilePicture = "res://social/connectpeople/fasterpc.png",
+                    Content = "You know what's also miserable? YOUR COMPUTER WITH ALL THAT GARBAGE! DOWNLOAD FASTERPC TODAY",
+                    Link = "fasterpc.com"
+                },
+
+                new MksPost {
+                    User = "Rob Smith",
+                    ProfilePicture = "res://os/assets/userIcons/duck.png",
+                    Content = "True... (FASTER PC IS MALWARE)"
+                },
+            }
+        },
+
+        new MksPost {
+            User = "Daniel McLeinad",
+            ProfilePicture = "res://os/assets/userIcons/pancakes.png",
+            // translators: james vs the stars is a reference to ross tibeeth, you probably already translated it
+            Content = "just listened to james vs the stars, does anybody genuinely like this shit?",
+            Replies = new MksPost[] {
+                new MksPost {
+                    User = "Daniel McLeinad",
+                    ProfilePicture = "res://os/assets/userIcons/pancakes.png",
+                    Content = "link here by the way",
+                    Link = "rosstibeeth.com"
+                },
+
+                new MksPost {
+                    User = "Faster PC",
+                    ProfilePicture = "res://social/connectpeople/fasterpc.png",
+                    Content = "You know what's something that people genuinely like? FASTERPC! CLEAN YOUR PC TODAY!",
+                    Link = "fasterpc.com"
+                },
+
+                new MksPost {
+                    User = "Ross Tibeeth (.com)",
+                    ProfilePicture = "res://web/rosstibeeth.com/assets/rossHimself.png",
+                    // translators: this is an answer to "does anybody genuinely like this shit?"
+                    Content = "I do"
+                },
+
+                new MksPost {
+                    User = "Rob Smith",
+                    ProfilePicture = "res://os/assets/userIcons/duck.png",
+                    Content = "<ping>Faster PC</ping> MALWARE"
+                },
+            }
+        },
+
+        new MksPost {
+            User = "FasterPC",
+            ProfilePicture = "res://social/connectpeople/fasterpc.png",
+            Content = "Slow PC? Use FasterPC! It's free!",
+            Link = "fasterpc.org",
+            Replies = new MksPost[] {
+                new MksPost {
+                    User = "Rob Smith",
+                    ProfilePicture = "res://os/assets/userIcons/duck.png",
+                    Content = "MALWARE!!!"
+                },
+
+                new MksPost {
+                    User = "FasterPC",
+                    ProfilePicture = "res://social/connectpeople/fasterpc.png",
+                    Content = "<ping>Rob Smith</ping> What?"
+                },
+
+                new MksPost {
+                    User = "Rob Smith",
+                    ProfilePicture = "res://os/assets/userIcons/duck.png",
+                    Content = "<ping>FasterPC</ping> your app shows ads that scam people"
+                },
+
+                new MksPost {
+                    User = "Faster PC",
+                    ProfilePicture = "res://social/connectpeople/fasterpc.png",
+                    Content = "<ping>FasterPC</ping> is a virus, <ping>Faster PC</ping> (this account) is the official!!",
+                    Link = "fasterpc.com"
                 },
             }
         },
