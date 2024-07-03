fbf39a095b1c036910f210dd0ff196f7bca52c8b
hellory4n <hellory4n@gmail.com>
2024-07-03 16:47:57 -0500

    Life is a constant state of suffering

--- os/kickstart/network/HyperConnectinator3000.cs
+++ os/kickstart/network/HyperConnectinator3000.cs
@@ -8,66 +8,65 @@ namespace markisa.network {
 
 public class HyperConnectinator3000 : Node
 {
-    public static Dictionary<PostZone, MksPost> Index { get; private set; } = new Dictionary<PostZone, MksPost>();
+    public static Dictionary<PostZone, List<MksPost>> Index { get; private set; } = new Dictionary<PostZone, List<MksPost>>() { {PostZone.Comedy, new List<MksPost>()}, {PostZone.Creativity, new List<MksPost>()}, {PostZone.Discussion, new List<MksPost>()}, {PostZone.Entertainment, new List<MksPost>()}, {PostZone.HelpCenter, new List<MksPost>()}, {PostZone.Technology, new List<MksPost>()} };
+    //public static Dictionary<PostZone, List<MksPost>> Index { get; private set; } = 
 
     public static string MurdersOfMurderers()
     {
         // first fuck around
         foreach (var post in TrendingZone1.Data.Posts) {
             foreach (var tłowšvryž in post.Zones) {
-                // fix thy shit
-                Index.Add(tłowšvryž, post);
+                Index[tłowšvryž].Add(post);
             }
         }
 
         // then find out
-        string code = "new Dictionary<string, HashSet<string>>() { ";
-        foreach (var gjdfjijgjdgjf in Index) {
-            code += $"{{ PostZone.{gjdfjijgjdgjf.Key}, new MksPost {{ ";
-            // KILL ME
-            code += $"User = \"{gjdfjijgjdgjf.Value.User}\", ";
-            code += $"ProfilePicture = \"{gjdfjijgjdgjf.Value.ProfilePicture}\", ";
-            code += $"Verified = \"{(gjdfjijgjdgjf.Value.Verified ? "true" : "false")}\", ";
-            code += $"Content = \"{gjdfjijgjdgjf.Value.Content.Replace("\"", "\\\"")}\", ";
-            code += $"Link = \"{gjdfjijgjdgjf.Value.Link}\", ";
+        string code = "new Dictionary<PostZone, List<MksPost>>() { ";
+        foreach (var yikyhkjyky in Index) {
+            code += $"{{ PostZone.{yikyhkjyky.Key}, new List<MksPost> {{ ";
 
-            // The industrial revolution and
-            code += "Images = new string[] { ";
-            foreach (var fff in gjdfjijgjdgjf.Value.Images) {
-                code += $"\"{fff}\", ";
-            }
-            code += "}, ";
-
-            // its consequences have been
-            code += "Zones = new PostZones[] { ";
-            foreach (var fff in gjdfjijgjdgjf.Value.Zones) {
-                code += $"\"{fff}\", ";
-            }
-            code += "}, ";
-
-            // a disaster for the human race
-            code += "Replies = new MksPost[] { ";
-            foreach (var nm in gjdfjijgjdgjf.Value.Replies) {
+            foreach (var gjdfjijgjdgjf in yikyhkjyky.Value) {
                 code += "new MksPost { ";
-                code += $"User = \"{gjdfjijgjdgjf.Value.User}\", ";
-                code += $"ProfilePicture = \"{gjdfjijgjdgjf.Value.ProfilePicture}\", ";
-                code += $"Verified = \"{(gjdfjijgjdgjf.Value.Verified ? "true" : "false")}\", ";
-                code += $"Content = \"{gjdfjijgjdgjf.Value.Content.Replace("\"", "\\\"")}\", ";
-                code += $"Link = \"{gjdfjijgjdgjf.Value.Link}\", ";
+                // KILL ME
+                code += $"User = \"{gjdfjijgjdgjf.User}\", ";
+                code += $"ProfilePicture = \"{gjdfjijgjdgjf.ProfilePicture}\", ";
+                code += $"Verified = {(gjdfjijgjdgjf.Verified ? "true" : "false")}, ";
+                code += $"Content = \"{gjdfjijgjdgjf.Content.Replace("\"", "\\\"")}\", ";
+                code += $"Link = \"{gjdfjijgjdgjf.Link}\", ";
+
+                // The industrial revolution and
                 code += "Images = new string[] { ";
-                foreach (var fff in gjdfjijgjdgjf.Value.Images) {
-                    code += $"\"{fff}\", ";
-                }
-                code += "}, Zones = new PostZones[] { ";
-                foreach (var fff in gjdfjijgjdgjf.Value.Zones) {
+                foreach (var fff in gjdfjijgjdgjf.Images) {
                     code += $"\"{fff}\", ";
                 }
                 code += "}, ";
-                code += "} ";
-            }
-            code += "} ";
 
-            code += "}";
+                // its consequences have been
+                code += "Zones = new PostZone[] { ";
+                foreach (var fff in gjdfjijgjdgjf.Zones) {
+                    code += $"PostZone.{fff}, ";
+                }
+                code += "}, ";
+
+                // a disaster for the human race
+                code += "Replies = new MksPost[] { ";
+                foreach (var nm in gjdfjijgjdgjf.Replies) {
+                    code += "new MksPost { ";
+                    code += $"User = \"{gjdfjijgjdgjf.User}\", ";
+                    code += $"ProfilePicture = \"{gjdfjijgjdgjf.ProfilePicture}\", ";
+                    code += $"Verified = {(gjdfjijgjdgjf.Verified ? "true" : "false")}, ";
+                    code += $"Content = \"{gjdfjijgjdgjf.Content.Replace("\"", "\\\"")}\", ";
+                    code += $"Link = \"{gjdfjijgjdgjf.Link}\", ";
+                    code += "Images = new string[] { ";
+                    foreach (var fff in gjdfjijgjdgjf.Images) {
+                        code += $"\"{fff}\", ";
+                    }
+                    code += "}, }, ";
+                }
+                code += "}, }, ";
+            }
+
+            code += "}, ";
         }
         code += " }";
         Index.Clear();
