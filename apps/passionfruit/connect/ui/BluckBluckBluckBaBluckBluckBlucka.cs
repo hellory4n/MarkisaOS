1a6e9bac2b5f2cfd61a6fa7f8d1f0af0c2e0bd06
hellory4n <hellory4n@gmail.com>
2024-07-03 22:01:38 -0500

    i'm gonna die

--- apps/passionfruit/connect/ui/BluckBluckBluckBaBluckBluckBlucka.cs
+++ apps/passionfruit/connect/ui/BluckBluckBluckBaBluckBluckBlucka.cs
@@ -9,19 +9,30 @@ public class BluckBluckBluckBaBluckBluckBlucka : Button
 {
     public override void _Ready()
     {
+        GD.Print("M");
         Visible = false;
+        GD.Print("N");
         #if DEBUG
+        GD.Print("B");
         Visible = true;
+        GD.Print("V");
         #endif
+        GD.Print("C");
     }
 
     public override void _Pressed()
     {
+        GD.Print("Q");
         #if DEBUG
+        GD.Print("W");
         string g = HyperConnectinator3000.MurdersOfMurderers();
+        GD.Print("E");
         OS.Clipboard = g;
+        GD.Print("R");
         #endif
+        GD.Print("T");
         Frambos.Notify("lmao", "lamo");
+        GD.Print("Y");
     }
 }
 
--- os/kickstart/network/HyperConnectinator3000.cs
+++ os/kickstart/network/HyperConnectinator3000.cs
@@ -1,76 +1,141 @@
 using Godot;
-using markisa.mkstoolkit;
-using System;
 using System.Collections.Generic;
+using System.Text;
+using markisa.kickstart;
+using System;
 using System.Linq;
 
-namespace markisa.network {
-
-public class HyperConnectinator3000 : Node
+namespace markisa.network
 {
-    public static Dictionary<PostZone, List<MksPost>> Index { get; private set; } = new Dictionary<PostZone, List<MksPost>>() { {PostZone.Comedy, new List<MksPost>()}, {PostZone.Creativity, new List<MksPost>()}, {PostZone.Discussion, new List<MksPost>()}, {PostZone.Entertainment, new List<MksPost>()}, {PostZone.HelpCenter, new List<MksPost>()}, {PostZone.Technology, new List<MksPost>()} };
+
+    public class HyperConnectinator3000 : Node
+{
+    public static Dictionary<PostZone, List<List<MksPost>>> Index { get; private set; } = new Dictionary<PostZone, List<List<MksPost>>>();
     //public static Dictionary<PostZone, List<MksPost>> Index { get; private set; } = 
 
     public static string MurdersOfMurderers()
     {
-        // first fuck around
+        GD.Print("Seroa;oze -3");
+        Index = new Dictionary<PostZone, List<List<MksPost>>>() {
+            {PostZone.Comedy, new List<List<MksPost>>()},
+            {PostZone.Creativity, new List<List<MksPost>>()},
+            {PostZone.Discussion, new List<List<MksPost>>()},
+            {PostZone.Entertainment, new List<List<MksPost>>()},
+            {PostZone.HelpCenter, new List<List<MksPost>>()},
+            {PostZone.Technology, new List<List<MksPost>>()},
+        };
+
+        // first fuck around (do the zone shit)
+        GD.Print("Seroa;oze -2");
         foreach (var post in TrendingZone1.Data.Posts) {
             foreach (var tłowšvryž in post.Zones) {
-                Index[tłowšvryž].Add(post);
+                if (Index[tłowšvryž].Count == 0) {
+                    Index[tłowšvryž].Add(new List<MksPost>() { post });
+                }
+                else {
+                    Index[tłowšvryž][0].Add(post);
+                }
+            }
+        }
+
+        // then still fuck around (split shit)
+        GD.Print("Seroa;oze -1");
+        foreach (var tłowšvryž in Index) {
+            // so random!
+            var random = new Random();
+            tłowšvryž.Value[0] = tłowšvryž.Value[0]
+                .Select(x => (x, random.Next()))
+                .OrderBy(tuple => tuple.Item2)
+                .Select(tuple => tuple.x)
+                .ToList();
+
+            var HEHE = new List<MksPost>();
+            int fuckoff = 0;
+            foreach (var fdfdfdfdfd in tłowšvryž.Value[0]) {
+                HEHE.Add(fdfdfdfdfd);
+                fuckoff++;
+
+                if (fuckoff == 8) {
+                    MksPost[] kfjkfkkf = new MksPost[HEHE.Count];
+                    HEHE.CopyTo(kfjkfkkf);
+                    tłowšvryž.Value.Add(kfjkfkkf.ToList());
+
+                    HEHE.Clear();
+                    fuckoff = 0;
+                }
             }
         }
 
         // then find out
-        string code = "new Dictionary<PostZone, List<MksPost>>() { ";
-        foreach (var yikyhkjyky in Index) {
-            code += $"{{ PostZone.{yikyhkjyky.Key}, new List<MksPost> {{ ";
+        GD.Print("Seroa;oze 0");
+        return Serialize(Index);
+    }
 
-            foreach (var gjdfjijgjdgjf in yikyhkjyky.Value) {
-                code += "new MksPost { ";
-                // KILL ME
-                code += $"User = \"{gjdfjijgjdgjf.User}\", ";
-                code += $"ProfilePicture = \"{gjdfjijgjdgjf.ProfilePicture}\", ";
-                code += $"Verified = {(gjdfjijgjdgjf.Verified ? "true" : "false")}, ";
-                code += $"Content = \"{gjdfjijgjdgjf.Content.Replace("\"", "\\\"")}\", ";
-                code += $"Link = \"{gjdfjijgjdgjf.Link}\", ";
+    public static string Serialize(object obj)
+    {
+        GD.Print("Seroa;oze `1");
+        var sb = new StringBuilder();
+        SerializeObject(sb, obj, 0);
+        GD.Print("Seroa;oze2");
+        return sb.ToString();
+    }
 
-                // The industrial revolution and
-                code += "Images = new string[] { ";
-                foreach (var fff in gjdfjijgjdgjf.Images) {
-                    code += $"\"{fff}\", ";
-                }
-                code += "}, ";
-
-                // its consequences have been
-                code += "Zones = new PostZone[] { ";
-                foreach (var fff in gjdfjijgjdgjf.Zones) {
-                    code += $"PostZone.{fff}, ";
-                }
-                code += "}, ";
-
-                // a disaster for the human race
-                code += "Replies = new MksPost[] { ";
-                foreach (var nm in gjdfjijgjdgjf.Replies) {
-                    code += "new MksPost { ";
-                    code += $"User = \"{gjdfjijgjdgjf.User}\", ";
-                    code += $"ProfilePicture = \"{gjdfjijgjdgjf.ProfilePicture}\", ";
-                    code += $"Verified = {(gjdfjijgjdgjf.Verified ? "true" : "false")}, ";
-                    code += $"Content = \"{gjdfjijgjdgjf.Content.Replace("\"", "\\\"")}\", ";
-                    code += $"Link = \"{gjdfjijgjdgjf.Link}\", ";
-                    code += "Images = new string[] { ";
-                    foreach (var fff in gjdfjijgjdgjf.Images) {
-                        code += $"\"{fff}\", ";
-                    }
-                    code += "}, }, ";
-                }
-                code += "}, }, ";
-            }
-
-            code += "}, ";
+    private static void SerializeObject(StringBuilder sb, object obj, int indentLevel)
+    {
+        GD.Print("Seroa;oze ibhect 1");
+        if (obj == null)
+        {
+            sb.Append("null");
+            return;
         }
-        code += " }";
-        Index.Clear();
-        return code;
+
+        GD.Print("Seroa;oze ibhect 2");
+        var type = obj.GetType();
+        if (type.IsPrimitive || obj is string)
+        {
+            sb.Append(obj.ToString());
+            return;
+        }
+
+        GD.Print("Seroa;oze ibhect 3");
+        if (obj is IEnumerable<object> list)
+        {
+            SerializeList(sb, list, indentLevel);
+            return;
+        }
+
+        GD.Print("Seroa;oze ibhect 4");
+        SerializeComplexObject(sb, obj, indentLevel);
+        GD.Print("Seroa;oze ibhect 5");
+    }
+
+    private static void SerializeList(StringBuilder sb, IEnumerable<object> list, int indentLevel)
+    {
+        GD.Print("Seroa;oze lsit 1");
+        sb.AppendLine("new List<object> {");
+        foreach (var item in list)
+        {
+            SerializeObject(sb, item, indentLevel + 1);
+            sb.AppendLine(",");
+        }
+        sb.Append("}");
+        GD.Print("Seroa;oze lsit 2");
+    }
+
+    private static void SerializeComplexObject(StringBuilder sb, object obj, int indentLevel)
+    {
+        GD.Print("Seroa;oze compelx oject 1");
+        var type = obj.GetType();
+        sb.AppendLine($"new {type.Name} {{");
+        foreach (var prop in type.GetProperties())
+        {
+            sb.Append($"{prop.Name} = ");
+            SerializeObject(sb, prop.GetValue(obj), indentLevel + 1);
+            sb.AppendLine(",");
+        }
+        GD.Print("Seroa;oze compelx oject 2");
+        sb.Append("}");
+        GD.Print("Seroa;oze compelx oject 3");
     }
 }
 
